0e9f6a42cc4d1c0b44662e7c574afac01776fdc9
Nicolas M <mine.nicolas@gmail.com>
2024-09-08 17:04:50 +0200

    test gallery

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        autoHeight: true,
+        imageSize: 'contain',
+        dots: true
+      } as GalleryConfig
+    }
+  ]
 };
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GalleryModule, GalleryItem, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [GalleryModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss'
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
+  images: ImageItem[] = [];
+
+  ngOnInit() {
+    this.images = [
+      new ImageItem({ src: 'photos/harbor.jpg', thumb: 'photos/harbor.jpg' }),
+      new ImageItem({ src: 'photos/hotel.jpg', thumb: 'photos/hotel.jpg' }),
+      new ImageItem({ src: 'photos/yb13.jpg', thumb: 'photos/yb13.jpg' }),
+      new ImageItem({ src: 'photos/iad.jpg', thumb: 'photos/iad.jpg' })
+    ];
+  }
 
 }
